d8dbc057448603415f159688d8f8300ebc313f82
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-07-02 16:59:00 +0530

    v1.0.0 Improved performance by large extent, Added script response handling.

--- youtubesearchpython/__init__.py
+++ youtubesearchpython/__init__.py
@@ -3,56 +3,53 @@ import urllib.request
 import json
 import html
 
-
 class searchYoutube:
 
     #########https://github.com/alexmercerind/youtube-search-python#########
 
-    networkError = False
-    validResponse = False
+    __networkError = False
+    __validResponse = False
 
     def __init__(self, keyword, offset = 1, mode = "json"):
 
         #########CLASS CONSTRUCTOR#########
-        #########https://github.com/alexmercerind/youtube-search-python#########
 
         #########Setting Feilds#########
 
-        self.keyword = urllib.parse.quote(keyword)
-        self.offset = offset
-        self.mode = mode
+        self.__keyword = urllib.parse.quote(keyword)
+        self.__offset = offset
+        self.__mode = mode
 
         #########Executing Entry Point Of Class#########
-        self.exec()
 
-    def request(self):
+        self.__exec()
+
+    def __request(self):
 
         try:
 
             #########Network request is packed in try: except: for network related error handling.#########
 
-            request = "https://www.youtube.com/results?search_query=%s&page=%d" %(self.keyword, self.offset)
+            request = "https://www.youtube.com/results?search_query=%s&page=%d" %(self.__keyword, self.__offset)
 
-            #########Making Network Request And Splitting It Into An Array.#########
+            #########Making Network Request#########
 
-            self.page = urllib.request.urlopen(request).read().decode('utf-8')
-            self.pageSource = self.page.split()
+            self.__page = urllib.request.urlopen(request).read().decode('utf-8')
 
-            #########Checking If The Valid Response Is Returned.#########
+            #########Identifying the type of response returned.#########
 
-            if self.page[0:29] == '  <!DOCTYPE html><html lang="':
-                self.validResponse = True
+            if self.__page[0:29] == '  <!DOCTYPE html><html lang="':
+                self.__validResponse = True
 
         except:
 
             #########Setting Network Error In Case Of Network Related Errors.#########
 
-            self.networkError = True
+            self.__networkError = True
 
-    def main(self):
+    def __scriptResponseHandler(self):
 
         #########MAIN PROPERTY#########
-        #########https://github.com/alexmercerind/youtube-search-python#########
 
         #########This property is later called in the another property exec() of the class.   #########
 
@@ -60,20 +57,102 @@ class searchYoutube:
 
         #########Defining Result Arrays.#########
 
-        self.links = []
-        self.ids = []
-        self.titles = []
-        self.views = []
-        self.durations = []
-        self.thumbnails = []
+        self.__links = []
+        self.__ids = []
+        self.__titles = []
+        self.__channels = []
+        self.__views = []
+        self.__durations = []
+        self.__thumbnails = []
 
         #########Transversing Through Network Request Array.#########
 
-        for index in range(0, len(self.pageSource)-1, 1):
+        self.__pageSource = self.__page.split('":"')
 
-            element = self.pageSource[index]
-            elementNext = self.pageSource[index+1]
-            elementPrev = self.pageSource[index-1]
+        for index in range(0, len(self.__pageSource)-1, 1):
+
+            #########Setting Video Links, IDs And Thumbnails.#########
+            if self.__pageSource[index][-98:] == '"commandMetadata":{"webCommandMetadata":{}},"addToPlaylistCommand":{"openMiniplayer":true,"videoId':
+                temp+=1
+                if temp % 2 == 0:
+                    id = self.__pageSource[index+1][0:11]
+                    thumbnailbuffer = []
+                    modes = ["default", "hqdefault", "mqdefault", "sddefault", "maxresdefault"]
+                    self.__ids+=[id]
+                    self.__links+=['https://www.youtube.com/watch?v='+ id]
+                    for mode in modes:
+                        thumbnailbuffer+=["https://img.youtube.com/vi/" + id + "/" + mode + ".jpg"]
+                    self.__thumbnails+=[thumbnailbuffer]
+            
+            #########Setting Video Channels, Titles, Views And Durations#########
+
+            if self.__pageSource[index][-44:] == '"accessibility":{"accessibilityData":{"label' and self.__pageSource[index+1][-36:] == '"descriptionSnippet":{"runs":[{"text':
+                infoString = '"' + self.__pageSource[index+1].split("}}}")[0]
+                titleBuffer = ""
+                channelBuffer = ""
+                durationBuffer = ""
+                durationBool = False
+                viewBuffer = 0
+                channelBool = True
+                separators = ["second", "hour", "day", "month", "year", "seconds", "hours", "days", "months", "years"]
+                timeBool = True
+                for element in infoString.split()[-2]:
+                    if element.isnumeric():
+                        viewBuffer = viewBuffer * 10 + int(element)
+                for index in range(len(infoString.split())):
+                    element = infoString.split()[index]
+                    if element != "by" and channelBool:
+                        titleBuffer+=element+" "
+                    if element == "by":
+                        channelBool = False
+                    if element != "by" and not channelBool and index < len(infoString.split())-1:
+                        if infoString.split()[index+1] in separators:
+                            timeBool = False
+                        else:
+                            if timeBool:
+                                channelBuffer+=element+" "
+                            else:
+                                break
+                for index in range(len(infoString.split())):
+                    if infoString.split()[index] == "ago":
+                        durationBool = True
+                        continue
+                    if durationBool:
+                        if index <= len(infoString.split())-3:
+                            durationBuffer += infoString.split()[index] + " "
+                        else:
+                            break
+                self.__channels+=[channelBuffer.rstrip().encode('utf-8').decode('unicode_escape')]
+                self.__titles+=[titleBuffer.rstrip()[1:].encode('utf-8').decode('unicode_escape')]
+                self.__views+=[viewBuffer]
+                self.__durations+=[durationBuffer.rstrip()]
+
+    def __pageResponseHandler(self):
+
+        #########MAIN PROPERTY#########
+
+        #########This property is later called in the another property exec() of the class.   #########
+
+        temp = 0
+
+        #########Defining Result Arrays.#########
+
+        self.__links = []
+        self.__ids = []
+        self.__titles = []
+        self.__views = []
+        self.__durations = []
+        self.__thumbnails = []
+
+        #########Transversing Through Network Request Array.#########
+
+        self.__pageSource = self.__page.split()
+
+        for index in range(0, len(self.__pageSource)-1, 1):
+
+            element = self.__pageSource[index]
+            elementNext = self.__pageSource[index+1]
+            elementPrev = self.__pageSource[index-1]
 
             #########Setting Video View Counts.#########
 
@@ -82,7 +161,7 @@ class searchYoutube:
                 for character in elementPrev:
                     if character.isnumeric():
                         viewCount = viewCount * 10 + int(character)
-                self.views+=[viewCount]            
+                self.__views+=[viewCount]            
 
             #########Setting Video Links, IDs And Thumbnails.#########
 
@@ -91,11 +170,11 @@ class searchYoutube:
                 modes = ["default", "hqdefault", "mqdefault", "sddefault", "maxresdefault"]
                 temp+=1
                 if temp%2 ==0:
-                    self.links+=['www.youtube.com'+element[6:len(element)-1]]
-                    self.ids+=[element[15:len(element) - 1]]
+                    self.__links+=['https://www.youtube.com'+element[6:len(element)-1]]
+                    self.__ids+=[element[15:len(element) - 1]]
                     for mode in modes:
                         thumbnailbuffer+=["https://img.youtube.com/vi/" + element[15:len(element) - 1] + "/" + mode + ".jpg"]
-                    self.thumbnails+=[thumbnailbuffer]
+                    self.__thumbnails+=[thumbnailbuffer]
 
             #########Setting Video Durations.#########
 
@@ -109,76 +188,62 @@ class searchYoutube:
                         buffer+=character
                     if character == "<":
                         break
-                self.durations+=[buffer[1::]]
+                self.__durations+=[buffer[1::]]
 
             #########Setting Video Titles.#########
 
             if (element[0:23] == 'data-sessionlink="itct=') and (elementNext[0:7] == 'title="'):
                 buffer = ""
-                init = self.pageSource[index+1]
+                init = self.__pageSource[index+1]
                 buffer+=init
                 subIndex = index+2
                 end = index+22
                 while subIndex<end:
-                    this_element = self.pageSource[subIndex]
-                    next_element = self.pageSource[subIndex+1]
+                    this_element = self.__pageSource[subIndex]
+                    next_element = self.__pageSource[subIndex+1]
                     if (this_element[len(this_element)-1])== '"':
                         if next_element == 'rel="spf-prefetch"':
                             buffer+=(" "+this_element)
-                            self.titles+=[html.unescape(buffer[7:-1])]
+                            self.__titles+=[html.unescape(buffer[7:-1])]
                             break
                     else:
                         buffer+=(" "+this_element)
                     subIndex+=1
 
-    def exec(self):
+    def __exec(self):
         
         #########EXEC PROPERTY#########
-        #########https://github.com/alexmercerind/youtube-search-python#########
         
         #########We are calling main property within this exec property because, YouTube randomly returns two types of#########
         #########responses, one having content as HTML and another as script, and this algorithm is designed to work  #########
-        #########with only HTML one. So, we have no choice but to just look if the algorithm failed i.e               #########
-        #########self.validResponse = False then again make the request and see the output, if we get valid response, #########
-        #########we call self.main() and return result to the user, instead of making request again.                  #########     
+        #########with both of them. So, we have no choice but to just look if the script response is recieved i.e     #########
+        #########self.validResponse = False then we execute self.scriptResponseHandler() instead of                   #########
+        #########self.pageResponseHandler(), finally, we call self.main() and return result to the user.              #########     
 
-        #########This causes heavy performance expenses but we have no choice at the point.#########
         #########We will seek potential fixes in future.#########
 
         #########Calling the main property.#########
 
-        self.request()
+        self.__request()
 
-        #########Checking for netWork error.#########
-
-        if self.networkError:
-
-            self.networkError = True
+        if self.__networkError:
+            self.__networkError = True
 
         else:
+
+            if not self.__validResponse:
+                self.__scriptResponseHandler()
             
-            #########Checking if we got bad response from YouTube.#########
-
-            if not self.validResponse:
-
-                #########Again called exec() in case of invalid response.#########
-
-                self.exec()
-            
-            if self.validResponse:
-                
-                #########Calling main() in case of a valid response.#########
-
-                self.main()
+            if self.__validResponse:
+                self.__pageResponseHandler()
 
     def result(self):
 
         #########RESULT PROPERTY#########
-        #########https://github.com/alexmercerind/youtube-search-python#########
 
         #########Checking for network error and returning None to the user in case of it.#########
 
-        if self.networkError:
+        if self.__networkError:
             return None
         
         #########Returning Result.#########
@@ -186,39 +251,64 @@ class searchYoutube:
         else:
 
             result = []
-
+            
             #########JSON Result Handling.#########
 
-            if self.mode == "json":
+            if self.__mode == "json":
 
-                for index in range(len(self.links) - 1):
-                    thisResult = {
+                for index in range(len(self.__titles) - 1):
+                    if not self.__validResponse:
+                        thisResult = {
                         "index": index,
-                        "id": self.ids[index],
-                        "link": self.links[index],
-                        "title": self.titles[index],
-                        "duration": self.durations[index],
-                        "views": self.views[index],
-                        "thumbnails": self.thumbnails[index]
+                        "id": self.__ids[index],
+                        "link": self.__links[index],
+                        "title": self.__titles[index],
+                        "channel": self.__channels[index],
+                        "duration": self.__durations[index],
+                        "views": self.__views[index],
+                        "thumbnails": self.__thumbnails[index]
                     }
+                    else:
+                        thisResult = {
+                        "index": index,
+                        "id": self.__ids[index],
+                        "link": self.__links[index],
+                        "title": self.__titles[index],
+                        "duration": self.__durations[index],
+                        "views": self.__views[index],
+                        "thumbnails": self.__thumbnails[index]
+                        }
                     result+=[thisResult]
 
                 return json.dumps({"search_result": result}, indent=4)
             
             #########List Result Handling.#########
 
-            elif self.mode == "list":
+            elif self.__mode == "list":
                 
-                for index in range(len(self.links) - 1):
-                    thisResult=[
-                        index,
-                        self.ids[index],
-                        self.links[index],
-                        self.titles[index],
-                        self.durations[index],
-                        self.titles[index],
-                        self.thumbnails[index]
-                    ]
+                for index in range(len(self.__titles) - 1):
+                    if not self.__validResponse:
+                        thisResult=[
+                            index,
+                            self.__ids[index],
+                            self.__links[index],
+                            self.__titles[index],
+                            self.__channels[index],
+                            self.__durations[index],
+                            self.__titles[index],
+                            self.__thumbnails[index]
+                        ]
+                    else:
+                        thisResult=[
+                            index,
+                            self.__ids[index],
+                            self.__links[index],
+                            self.__titles[index],
+                            self.__durations[index],
+                            self.__titles[index],
+                            self.__thumbnails[index]
+                        ]
+
                     result+=[thisResult]
                 
                 return result
