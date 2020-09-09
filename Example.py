8b69537a8666a21dd82c479d91cf301831074e3a
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-09 14:19:30 +0530

    📐 Updated example

--- Example.py
+++ Example.py
@@ -2,11 +2,11 @@ import asyncio
 from youtubesearchpython import SearchVideos, SearchPlaylists
 
 async def main():
-    youtube = SearchVideos('NoCopyrightSounds', offset = 1, mode = 'json', max_results = 20)
+    youtube = SearchVideos('NoCopyrightSounds', offset = 1, mode = 'json', maxResults = 20)
     await youtube.search()
     print(youtube.result())
 
-    playlists = SearchPlaylists('NoCopyrightSounds', offset = 1, mode = 'json', max_results = 20)
+    playlists = SearchPlaylists('NoCopyrightSounds', offset = 1, mode = 'json', maxResults = 20)
     await playlists.search()
     print(playlists.result())
 
