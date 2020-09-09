4ae20f83ba074a9e2e73efd953ec93ee05293f13
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-09 13:49:52 +0530

    👌 Added type annotations & docstrings

--- youtubesearchpython/playlist__search.py
+++ youtubesearchpython/playlist__search.py
@@ -1,85 +1,106 @@
 import json
+import asyncio
 
-from youtubesearchpython.__requesthandler import requesthandler
-from youtubesearchpython.playlist__scripthandler import scripthandler
+from youtubesearchpython.__requesthandler import RequestHandler
+from youtubesearchpython.playlist__scripthandler import ScriptHandler
 
 
-class SearchPlaylists(requesthandler, scripthandler):
+class SearchPlaylists(RequestHandler, ScriptHandler):
 
-    #########https://github.com/alexmercerind/youtube-search-python#########
+    '''
+    Search for playlists in YouTube.
+
+    Parameters
+    ----------
+    keyword : str
+        Used as a query to search for playlists in YouTube.
+    offset : int, optional
+        Offset for result pages on YouTube. Defaults to 1.
+    mode : str
+        Search result mode. Can be 'json', 'dict' or 'list'.
+    max_results : int, optional
+        Maximum number of playlist results. Defaults to 20.
+
+    Methods
+    -------
+    search()
+        Searches for the playlists in YouTube
+    result()
+        Returns the playlists fetched from YouTube by search().
+    '''
 
     networkError = False
     validResponse = False
+    searchComplete = False
 
-    def __init__(self, keyword, offset = 1, mode = "json", max_results = 20):
-
-        #########CLASS CONSTRUCTOR#########
-
-        #########Setting Feilds#########
+    def __init__(self, keyword: str, offset : int = 1, mode : str = 'json', max_results : int = 20):
 
         self.offset = offset
         self.mode = mode
         self.keyword = keyword
-        self.max_results = max_results
-        self.searchPreferences = "EgIQAw%3D%3D"
+        self.maxResults = max_results
+        self.searchPreferences = 'EgIQAw%3D%3D'
 
-        #########mainuting Entry Point Of Class#########
-
-        self.main()
-
-    def main(self):
+    async def search(self):
         
-        #########main PROPERTY#########
-
-        self.request()
+        await self.request()
 
         if self.networkError:
             self.networkError = True
         else:
             self.scriptResponseHandler()
+        self.searchComplete = True
 
     def result(self):
 
-        #########result PROPERTY#########
+        '''
+        Returns
+        -------
+        None, str, dict, list
+            Returns playlist results from YouTube. Returns None if self.search() is not awaited or called.
+        '''
 
-        #########Checking for network error and returning None to the user in case of it.#########
+        if self.searchComplete:
+            if self.networkError:
+                return None
 
-        if self.networkError:
+            else:
+
+                result = []
+                
+                if self.mode in ['json', 'dict']:
+
+                    for index in range(len(self.ids)):
+                        result_index = {
+                            'index': index,
+                            'id': self.ids[index],
+                            'link': self.links[index],
+                            'title': self.titles[index],
+                        }
+                        result+=[result_index]
+
+                    if self.mode == 'json':
+                        return json.dumps({'search_result': result}, indent=4)
+                    else:
+                        return {'search_result': result}
+                
+                elif self.mode == 'list':
+                    
+                    for index in range(len(self.ids)):
+                        list_index=[
+                            index,
+                            self.ids[index],
+                            self.links[index],
+                            self.titles[index],
+                        ]
+                        result+=[list_index]
+                    
+                    return result
+        else:
             return None
 
-        else:
+    def __enter__(self):
+        return self
 
-            result = []
-            
-            #########JSON Result Handling.#########
-
-            if self.mode in ["json", "dict"]:
-
-                for index in range(len(self.ids)):
-                    result_index = {
-                        "index": index,
-                        "id": self.ids[index],
-                        "link": self.links[index],
-                        "title": self.titles[index],
-                    }
-                    result+=[result_index]
-
-                if self.mode == "json":
-                    return json.dumps({"search_result": result}, indent=4)
-                else:
-                    return {"search_result": result}
-            
-            #########List Result Handling.#########
-
-            elif self.mode == "list":
-                
-                for index in range(len(self.ids)):
-                    list_index=[
-                        index,
-                        self.ids[index],
-                        self.links[index],
-                        self.titles[index],
-                    ]
-                    result+=[list_index]
-                
-                return result
+    def __exit__(self, execType, execValue, traceback):
+        pass
