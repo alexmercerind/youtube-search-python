a019d5814aa5de0391c36b3525f8b1f38c6ba403
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-09 13:54:26 +0530

    🔖 youtube-search-python v1.2.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="youtube-search-python",
-    version="1.2.8",
+    version="1.2.9",
     author="Hitesh Kumar Saini",
     author_email="saini123hitesh@gmail.com",
     description="Search for YouTube videos WITHOUT YouTube Data API v3",
@@ -14,9 +14,8 @@ setuptools.setup(
     url="https://github.com/alexmercerind/youtube-search-python",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.3,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=3.4',
 )
