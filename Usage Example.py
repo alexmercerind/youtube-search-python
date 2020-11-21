cc7b99148fceadab69a273f73a65694b39f4a761
alexmercerind <saini123hitesh@gmail.com>
2020-11-21 21:51:59 +0530

    🔧 Updated usage example

--- Usage Example.py
+++ Usage Example.py
@@ -1,9 +1,23 @@
 from youtubesearchpython import SearchVideos, SearchPlaylists
 
-videos = SearchVideos("NoCopyrightSounds", offset = 1, mode = "json", max_results = 20)
+videos = SearchVideos(
+    "NoCopyrightSounds",
+    offset = 1,
+    mode = "json",
+    max_results = 20,
+    language = "en-US",
+    region="US"
+)
 videosResult = videos.result()
 print(videosResult)
 
-playlists = SearchPlaylists("NoCopyrightSounds", offset = 1, mode = "json", max_results = 20)
+playlists = SearchPlaylists(
+    "NoCopyrightSounds",
+    offset = 1,
+    mode = "json",
+    max_results = 20,
+    language = "ru-RU",
+    region="US"
+)
 playlistsResult = playlists.result()
 print(playlistsResult)
