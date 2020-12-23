f86edc541b68070c6d613fcf3a68ceab1520a669
alexmercerind <saini123hitesh@gmail.com>
2020-12-23 18:48:56 +0530

    ✒ Updated example

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ Searches for all types of results like videos, channels & playlists in YouTube.
 
 'type' key in the JSON/Dictionary can be used to differentiate between the types of result.
 '''
-allSearch = Search('NoCopyrightSounds', page = 1, limit = 1, language = 'en-US', region = 'US')
+allSearch = Search('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
 print(allSearch.result())
 
 
@@ -17,9 +17,9 @@ print(allSearch.result())
 '''
 Searches only for videos in YouTube.
 '''
-videosSearch = VideosSearch('NoCopyrightSounds', page = 1, limit = 10, language = 'en-US', region = 'US')
+videosSearch = VideosSearch('NoCopyrightSounds', limit = 10, language = 'en', region = 'US')
 
-print(videosSearch.result())
+print(videosSearch.result(mode = ResultMode.json))
 
 
 
@@ -27,11 +27,11 @@ print(videosSearch.result())
 '''
 Searches only for channels in YouTube.
 '''
-channelsSearch = ChannelsSearch('NoCopyrightSounds', page = 1, limit = 1, language = 'en-US', region = 'US')
+channelsSearch = ChannelsSearch('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
 '''
 Setting mode = ResultMode.dict for getting dictionary result instead of JSON. Default is ResultMode.json.
 '''
-print(channelsSearch.result(mode = ResultMode.dict))
+print(channelsSearch.result(mode = ResultMode.json))
 
 
 
@@ -39,14 +39,14 @@ print(channelsSearch.result(mode = ResultMode.dict))
 '''
 Searches only for playlists in YouTube.
 '''
-playlistsSearch = PlaylistsSearch('NoCopyrightSounds', page = 1, limit = 1, language = 'en-US', region = 'US')
+playlistsSearch = PlaylistsSearch('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
 print(playlistsSearch.result())
 
 
 
 
 '''
-Can be used to get custom search results with defined filters.
+Can be used to get search results with custom defined filters.
 
 Setting second parameter as VideoSortOrder.uploadDate, to get video results sorted according to upload date.
 
@@ -60,10 +60,32 @@ There are many other for you to check out.
 If this much control isn't enough then, you can pass custom string yourself by seeing the YouTube query in any web browser e.g. 
 "EgQIBRAB" from "https://www.youtube.com/results?search_query=NoCopyrightSounds&sp=EgQIBRAB" can be passed as second parameter to get only videos, which are uploaded this year.
 '''
-customSearch = CustomSearch('NoCopyrightSounds', VideoSortOrder.uploadDate, limit = 1, language = 'en-US', region = 'US')
+customSearch = CustomSearch('NoCopyrightSounds', VideoSortOrder.uploadDate, language = 'en', region = 'US')
 print(customSearch.result())
 
 
+
+
+'''
+Getting search results from the next pages on YouTube.
+Generally you'll get maximum of 20 videos in one search, for getting subsequent results, you may call [next] method.
+'''
+
+search = VideosSearch('NoCopyrightSounds')
+index = 0
+for video in search.result()['result']:
+    print(str(index) + ' - ' + video['title'])
+    index += 1
+'''Getting result on 2nd page.'''
+search.next()
+for video in search.result()['result']:
+    print(str(index) + ' - ' + video['title'])
+    index += 1
+'''Getting result on 3rd page.'''
+search.next()
+for video in search.result()['result']:
+    print(str(index) + ' - ' + video['title'])
+    index += 1
 
 
 '''
