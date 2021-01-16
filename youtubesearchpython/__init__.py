17276fb39d74de34cc63cc4c2b9405c24815f79e
alexmercerind <saini123hitesh@gmail.com>
2021-01-16 12:48:47 +0530

    🔖 Bumped version to 1.3.6

--- youtubesearchpython/__init__.py
+++ youtubesearchpython/__init__.py
@@ -1,10 +1,11 @@
 from youtubesearchpython.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch
 from youtubesearchpython.extras import Video, Suggestions
+from youtubesearchpython.streamurlfetcher import StreamURLFetcher
 from youtubesearchpython.internal.constants import *
 
 
 __title__        = 'youtube-search-python'
-__version__      = '1.3.5'
+__version__      = '1.3.6'
 __author__       = 'alexmercerind'
 __license__      = 'MIT'
 
