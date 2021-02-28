0bdc248aec38d7bfb1d47eb4c6ab693fb18572a6
alexmercerind <saini123hitesh@gmail.com>
2021-02-28 20:31:32 +0530

    ⚡ Added Playlist class with async support

--- youtubesearchpython/__future__/internal/extras.py
+++ youtubesearchpython/__future__/internal/extras.py
@@ -109,6 +109,110 @@ class VideoInternal:
         return value
 
 
+class PlaylistInternal:
+
+    def __init__(self, playlistLink: str, componentMode: str):
+        self.playlistLink = playlistLink
+        self.componentMode = componentMode
+    
+    async def get(self):
+        statusCode = await self.__makeRequest(self.playlistLink)
+        await self.__getComponents()
+
+    async def __makeRequest(self, playlistLink: str) -> int:
+        playlistLink.strip('/')
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    playlistLink,
+                    params = {
+                        'pbj': '1',
+                    },
+                )
+                self.responseSource = response.json()
+        except:
+            raise Exception('ERROR: Could not make request.')
+
+    async def __getComponents(self) -> None:
+        for response in self.responseSource:
+            if 'response' in response.keys():
+                playlistElement = {
+                    'info':                               await self.__getValue(response, playlistInfoPath),
+                    'videos':                             await self.__getValue(response, playlistVideosPath),
+                }
+                if not playlistElement['info']:
+                    raise Exception('ERROR: Could not parse YouTube response.')
+                self.playlistComponent = await self.__getPlaylistComponent(playlistElement, self.componentMode)
+                
+    async def __getPlaylistComponent(self, element: dict, mode: str) -> dict:
+        playlistComponent = {}
+        if mode in ['getInfo', None]:
+            for infoElement in element['info']:
+                if playlistPrimaryInfoKey in infoElement.keys():
+                    component = {
+                        'id':                             await self.__getValue(infoElement, [playlistPrimaryInfoKey, 'title', 'runs', 0, 'navigationEndpoint', 'watchEndpoint', 'playlistId']),
+                        'title':                          await self.__getValue(infoElement, [playlistPrimaryInfoKey, 'title', 'runs', 0, 'text']),
+                        'videoCount':                     await self.__getValue(infoElement, [playlistPrimaryInfoKey, 'stats', 0, 'runs', 0, 'text']),
+                        'viewCount':                      await self.__getValue(infoElement, [playlistPrimaryInfoKey, 'stats', 1, 'simpleText']),
+                        'thumbnails':                     await self.__getValue(infoElement, [playlistPrimaryInfoKey, 'thumbnailRenderer', 'playlistVideoThumbnailRenderer', 'thumbnail']),
+                    }
+                    if not component['thumbnails']:
+                        component['thumbnails'] =         self.__getValue(infoElement, [playlistPrimaryInfoKey, 'thumbnailRenderer', 'playlistCustomThumbnailRenderer', 'thumbnail', 'thumbnails']),
+                    component['link'] = 'https://www.youtube.com/playlist?list=' + component['id']
+                    playlistComponent.update(component)
+                if playlistSecondaryInfoKey in infoElement.keys():
+                    component = {
+                        'channel': {
+                            'name':                       await self.__getValue(infoElement, [playlistSecondaryInfoKey, 'videoOwner', 'videoOwnerRenderer', 'title', 'runs', 0, 'text']),
+                            'id':                         await self.__getValue(infoElement, [playlistSecondaryInfoKey, 'videoOwner', 'videoOwnerRenderer', 'title', 'runs', 0, 'navigationEndpoint', 'browseEndpoint', 'browseId']),
+                            'thumbnails':                 await self.__getValue(infoElement, [playlistSecondaryInfoKey, 'videoOwner', 'videoOwnerRenderer', 'thumbnail', 'thumbnails']),
+                        },
+                    }
+                    component['channel']['link'] = 'https://www.youtube.com/channel/' + component['channel']['id']
+                    playlistComponent.update(component)
+        if mode in ['getVideos', None]:
+            playlistComponent['videos'] = []
+            for videoElement in element['videos']:
+                if playlistVideoKey in videoElement:
+                    videoComponent = {
+                        'id':                             await self.__getValue(videoElement, [playlistVideoKey, 'videoId']),
+                        'title':                          await self.__getValue(videoElement, [playlistVideoKey, 'title', 'runs', 0, 'text']),
+                        'thumbnails':                     await self.__getValue(videoElement, [playlistVideoKey, 'thumbnail', 'thumbnails']),
+                        'channel': {
+                            'name':                       await self.__getValue(videoElement, [playlistVideoKey, 'shortBylineText', 'runs', 0, 'text']),
+                            'id':                         await self.__getValue(videoElement, [playlistVideoKey, 'shortBylineText', 'runs', 0, 'navigationEndpoint', 'browseEndpoint', 'browseId']),
+                        },
+                        'duration':                       await self.__getValue(videoElement, [playlistVideoKey, 'lengthText', 'simpleText']),
+                        'accessibility': {
+                            'title':                      await self.__getValue(videoElement, [playlistVideoKey, 'title', 'accessibility', 'accessibilityData', 'label']),
+                            'duration':                   await self.__getValue(videoElement, [playlistVideoKey, 'lengthText', 'accessibility', 'accessibilityData', 'label']),
+                        },
+                    }
+                    videoComponent['link'] = 'https://www.youtube.com/watch?v=' + videoComponent['id']
+                    videoComponent['channel']['link'] = 'https://www.youtube.com/channel/' + videoComponent['channel']['id']
+                    playlistComponent['videos'].append(
+                        videoComponent
+                    )
+        return playlistComponent
+
+    async def __getValue(self, source: dict, path: List[str]) -> Union[str, int, dict, None]:
+        value = source
+        for key in path:
+            if type(key) is str:
+                if key in value.keys():
+                    value = value[key]
+                else:
+                    value = None
+                    break
+            elif type(key) is int:
+                if len(value) != 0:
+                    value = value[key]
+                else:
+                    value = None
+                    break
+        return value
+
+
 class SuggestionsInternal:
     searchSuggestions = []
     
