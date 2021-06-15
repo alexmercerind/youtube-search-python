3ccc79df050087f66756d26d5f6a1891d1695447
mytja <mytja@protonmail.com>
2021-06-15 08:15:08 +0200

    🔖 Bumped version to 1.4.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="youtube-search-python",
-    version="1.4.5",
+    version="1.4.6",
     author="Hitesh Kumar Saini",
     license = 'MIT',
     author_email="saini123hitesh@gmail.com",
