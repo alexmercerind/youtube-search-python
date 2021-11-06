e784a387bc30949e0a20636c2d2dbf5c364191b1
Felix Stupp <felix.stupp@outlook.com>
2021-11-06 17:40:25 +0100

    Video: Add duration, allowRatings, isLiveContent and isLiveNow
    - duration, allowRatings and isLiveContent exposure just more data the YouTube backend exposes to the client
    - isLiveContent means, that this video __was__ or is live streamed (probably useful for the UI as to know that the chat should be shown and so on)
    - isLiveNow is more complex as this property is interfered from duration equals 0 (probably no finite length known for now) and isLiveContent

--- youtubesearchpython/core/video.py
+++ youtubesearchpython/core/video.py
@@ -71,6 +71,9 @@ class VideoCore(RequestCore):
             component = {
                 'id': getValue(self.responseSource, ['videoDetails', 'videoId']),
                 'title': getValue(self.responseSource, ['videoDetails', 'title']),
+                'duration': {
+                    'secondsText': getValue(self.responseSource, ['videoDetails', 'lengthSeconds']),
+                },
                 'viewCount': {
                     'text': getValue(self.responseSource, ['videoDetails', 'viewCount'])
                 },
@@ -80,11 +83,14 @@ class VideoCore(RequestCore):
                     'name': getValue(self.responseSource, ['videoDetails', 'author']),
                     'id': getValue(self.responseSource, ['videoDetails', 'channelId']),
                 },
+                'allowRatings': getValue(self.responseSource, ['videoDetails', 'allowRatings']),
                 'averageRating': getValue(self.responseSource, ['videoDetails', 'averageRating']),
                 'keywords': getValue(self.responseSource, ['videoDetails', 'keywords']),
+                'isLiveContent': getValue(self.responseSource, ['videoDetails', 'isLiveContent']),
                 'publishDate': getValue(self.responseSource, ['microformat', 'playerMicroformatRenderer', 'publishDate']),
                 'uploadDate': getValue(self.responseSource, ['microformat', 'playerMicroformatRenderer', 'uploadDate']),
             }
+            component['isLiveNow'] = component['isLiveContent'] and component['duration']['secondsText'] == "0"
             component['link'] = 'https://www.youtube.com/watch?v=' + component['id']
             component['channel']['link'] = 'https://www.youtube.com/channel/' + component['channel']['id']
             videoComponent.update(component)
